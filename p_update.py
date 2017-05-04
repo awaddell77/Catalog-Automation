b72db63cf6964d82ff9cc5b74810b79a25834fa5
awaddell77 <waddell.andrew@gmail.com>
2017-05-04 12:01:01 -0700

    Started work on p_update program

---
+++ p_update.py
@@ -0,0 +1,10 @@
+#key match
+from Cat_dbase import *
+class Pupdate:
+	def __init__(self, category, new_info):
+		self.category = category
+		self.new_info = dictionarify(new_info)
+	def import_doc(self, x):
+		#for importing new file without having to start new instance
+		self.new_info = dictionarify(x)
+		
