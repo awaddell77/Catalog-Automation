ac125ef973c4b1df2bad2e722dca36f8e1e702be
awaddell77 <waddell.andrew@gmail.com>
2017-11-10 11:46:38 -0800

    Changed default limit from 10 to 3

--- Asin_update.py
+++ Asin_update.py
@@ -188,7 +188,7 @@ class Asin_update:
 			self.__prod_info.append(desc)
 		return self.get_prod_info()
 
-	def create_asins_v2(self, limit = 10):
+	def create_asins_v2(self, limit = 3):
 		#should keep on trying to add the single item
 		self.__retr_lst = []
 		self.__fail_lst = []
