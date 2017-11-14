08fa320a361f82d3b61f76dff730de356ab9d408
awaddell77 <waddell.andrew@gmail.com>
2017-11-14 11:28:54 -0800

    Manufacturer Part code randomizer

--- amazon_new.py
+++ amazon_new.py
@@ -59,6 +59,36 @@ class Asin_create(object):
 		return self.asins
 	def set_asins(self, x):
 		self.asins = x
+	def pn_randomizer(self, number, ch_limit = 0, new_number = True):
+		#generates random part code for amazon
+		#if ch_limit is greater than 0 it will add new characters to end of number
+		#if new number is true and ch_limit is greater than or equal to 1 it will generate a new part code altogether
+		newNumber = ""
+		for i in range(0, len(number)):
+			ch = random.randint(64, 123)
+			flip = random.randint(0, 10)
+			if flip > 5 and ch not in [64, 123] and ch not in range(91, 97):
+				newNumber += chr(ch)
+			else:
+				newNumber += str(number[i])
+		if ch_limit >= 1:
+			newNumber = ""
+			#print("TESTING")
+			for i in range(0, ch_limit):
+				ch = random.randint(64, 123)
+				if flip > 2 and ch not in [64, 123] and ch not in range(91, 97):
+					newNumber += chr(ch)
+				elif flip > 2 and flip <= 5:
+					newNumber += chr(random.randint(97, 123))
+				else:
+					newNumber += str(random.randint(1,9))
+			if new_number:
+				return newNumber
+			else:
+				return number + newNumber
+		return newNumber
+
+
 
 	def add_single(self, x ):
 		start_time = time.time()
@@ -71,6 +101,7 @@ class Asin_create(object):
 		#brand name
 		self.browser.js("document.getElementById('brand_name').value = '{0}'".format(prep(x["Manufacturer"])))
 		#manufacturer part number
+		x["Part Number"] = self.pn_randomizer(x["Part Number"], 2, False)
 		self.browser.js("document.getElementById('part_number').value = '{0}'".format(prep(x["Part Number"])))
 		#minimum age
 		self.browser.js("document.getElementById('mfg_minimum').value = '{0}'".format(prep(x["Ages"])))
